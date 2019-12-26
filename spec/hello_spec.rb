2b725d551f5fbc6bbc1d22673a90fbd67067bdb6
Tayna Rodrigues <taynajesus@gmail.com>
2019-12-25 22:34:11 -0400

    subindo o chrome e firefox

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -4,6 +4,6 @@ describe 'Meu primeiro script' do
     it 'visitar a pagina' do
 
         visit 'https://training-wheels-protocol.herokuapp.com/'
-        puts page.title
+        expect(page.title).to eql 'Training Wheels Protocol'
     end
 end

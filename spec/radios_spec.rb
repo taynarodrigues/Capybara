4ddbb135148cfad7cfa81038bfdf53181246122c
Tayna Rodrigues <taynajesus@gmail.com>
2019-12-29 19:32:32 -0400

    Botões de Rádio

---
+++ spec/radios_spec.rb
@@ -0,0 +1,22 @@
+
+describe 'Botões de Radio', :radio do
+    before(:each) do
+        visit 'https://training-wheels-protocol.herokuapp.com/radios'
+    end
+
+    it 'seleção por ID' do
+        choose('cap')
+    end
+
+    it 'seleção por find e css selector' do
+        find('input[value=guardians]').click
+    
+    end
+
+
+    after(:each) do
+        sleep 3
+    end
+
+
+end

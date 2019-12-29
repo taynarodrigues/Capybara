7fc63ea5a27db1abbfac7c1101d196fdfd2b7043
Tayna Rodrigues <taynajesus@gmail.com>
2019-12-29 16:34:19 -0400

    validando notificações

--- spec/forms_spec.rb
+++ spec/forms_spec.rb
@@ -5,11 +5,17 @@ describe 'Forms' do
 
         visit 'https://training-wheels-protocol.herokuapp.com/login'
 
-        fill_in 'username', with: 'stark'
+        fill_in 'userId', with: 'stark'
         fill_in 'password', with: 'jarvis!'
 
         click_button 'Login'
 
         expect(find('#flash').visible?).to be true
+
+        # puts find('#flash').text #imprimindo na tela
+
+        # expect(find('#flash').text).to include 'Olá, Tony Stark. Você acessou a área logada!'
+
+        expect(find('#flash')).to have_content 'Olá, Tony Stark. Você acessou a área logada!'
     end
 end

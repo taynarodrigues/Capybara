f88d3bac5519e5b09cce1cfda392daa817e914c9
Tayna Rodrigues <taynajesus@gmail.com>
2019-12-29 22:27:12 -0400

    Melhorando CSS Selectors com Regex 'expressão regular'

--- spec/hovers_spec.rb
+++ spec/hovers_spec.rb
@@ -6,21 +6,21 @@ describe 'Mouse Hover', :hover do
         end
 
         it 'quando passo o mouse no blade' do
-            card = find('img[alt=Blade]')
+            card = find('img[alt*=Blade]')
             card.hover
 
             expect(page).to have_content 'Nome: Blade'
         end
 
         it 'quando passo o mouse no pantera negra' do
-            card = find('img[alt="Pantera Negra"]')
+            card = find('img[alt^=Pantera]')
             card.hover
 
             expect(page).to have_content 'Nome: Pantera Negra'
         end
 
         it 'quando passo o mouse no homem aranha' do
-            card = find('img[alt="Homem Aranha"]')
+            card = find('img[alt$=Aranha]')
             card.hover
 
             expect(page).to have_content 'Nome: Homem Aranha'

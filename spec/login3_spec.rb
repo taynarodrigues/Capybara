aa5a1c38d72b24a2315eaa13b83663da7fcf1abd
Tayna Rodrigues <taynajesus@gmail.com>
2020-01-02 22:37:41 -0400

    Otimizando código: Escopos do Capybara

--- spec/login3_spec.rb
+++ spec/login3_spec.rb
@@ -7,11 +7,23 @@ describe 'Login com Cadastro', :login3 do
          
         it 'login com sucesso' do
 
-        login_form = find('#login')
-            login_form.find('input[name=username]').set 'stark'
-            login_form.find('input[name=password]').set 'jarvis!'
+            within('#login') do 
+                find('input[name=username]').set 'stark'
+                find('input[name=password]').set 'jarvis!'
+                click_button 'Entrar'
+        end
 
-            click_button 'Entrar'
         expect(find('#flash')).to have_content 'Olá, Tony Stark. Você acessou a área logada!'
-            end
+    end
+
+        it 'cadastro com sucesso' do
+            within('#signup') do
+                find('input[name=username').set 'fernando'
+                find('input[name=password').set '123456'
+                click_link 'Criar Conta'
+
+        end
+            expect(page).to have_content 'Dados enviados. Aguarde aprovação do seu cadastro!'
+                sleep 5
+        end
 end

d0d3c2df4504cadfb513f90539b25b5c0b8ce804
Tayna Rodrigues <taynajesus@gmail.com>
2020-01-02 23:06:45 -0400

    Buscando IDs Dinâmicos com expessão regular para encontrar elementos com CSS selector

--- spec/ids_dinamicos_spec.rb
+++ spec/ids_dinamicos_spec.rb
@@ -3,11 +3,17 @@ describe 'IDs Dinâmicos', :ids_dinamicos do
         visit 'https://training-wheels-protocol.herokuapp.com/access'
     end
 
+    # regra
+    #S => termina com
+    #^ => começa com
+    #* => contém
+
     it 'cadastro' do
-        find('PortalTheme_wt10_block_wtcontent_holder_wt8_wtUsernameInput').set 'fernado'
-        find('PasswordInput_wt11_PortalTheme_wt7_block_wtcontent_holder_wt8_wt').set '123456'
-        find('PortalTheme_wt4_block_wtGetStartedButton_wtcontent_holder_OSCore_wt3_block').click
+        find('input[id$=UsernameInput]').set 'fernado'
+        find('input[id^=PasswordInput]').set '123456'
+        find('a[id*=GetStarteButton]').click
 
         expect(page).to have_content 'Dados enviados. Aguarde aprovação do seu cadastro!'
     end
 end
+

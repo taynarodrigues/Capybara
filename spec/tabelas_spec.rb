084a631a4ab27d2015d9c5ab904d243e6fd7411a
Tayna Rodrigues <taynajesus@gmail.com>
2020-01-06 21:48:37 -0400

    refatorando código :verificando dados em tabelas

--- spec/tabelas_spec.rb
+++ spec/tabelas_spec.rb
@@ -4,12 +4,14 @@
         visit 'https://training-wheels-protocol.herokuapp.com/tables'
     end
 
-    it 'pega o tony stark na lista' do 
-        expect(page).to have_content 'Robert Downey Jr'
-    end
-
     it 'deve exibir o salario do stark' do
-        expect(page).to have_content '10.000.000'
+
+        atores = all('table tbody tr')
+
+        stark = atores.detect { |ator| ator.text.include?('Robert Downey Jr')}
+
+        expect(stark.text).to include '10.000.000'
+        
     end
 
  
